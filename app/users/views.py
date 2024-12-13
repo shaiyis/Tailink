c88769b7d47bce517134d899c87e374c5aebbe23
Israel Shai <shaiyis@gmail.com>
2024-12-13 15:02:07 +0200

    create user login api

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+# input username, password, output - AuthToken
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
