4aab9f30a10277d27fa830857368d2a792b937be
Israel Shai <shaiyis@gmail.com>
2024-12-22 21:11:56 +0200

    separate to 3 services, add swagger

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_spectacular',
     'profile',
     'place',
 ]
@@ -128,3 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('profile.urls'))
+    path('api/', include('profile.urls')),
+    path('schema', SpectacularAPIView.as_view(), name='api-schema'),
+    path('docs', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs')
 ]
